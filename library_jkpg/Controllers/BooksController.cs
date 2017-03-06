8bcab81e8ab10b877c2d2517487878cfa52316fc
A. Yanev <atanas.yanew@gmail.com>
2017-03-06 15:23:30 +0100

    [Authorize] /BooksController.cs

--- library_jkpg/Controllers/BooksController.cs
+++ library_jkpg/Controllers/BooksController.cs
@@ -11,14 +11,17 @@ using PagedList;
 
 namespace library_jkpg.Controllers
 {
-    [Authorize(Roles = "Administrator, Librarian")]
+    [Authorize]
     public class BooksController : Controller
     {
         private MyDbContext db = new MyDbContext();
 
         // GET: Books
-        [Authorize]
-        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page, int? show)
+        public ActionResult Index(string sortOrder, 
+                                  string currentFilter,
+                                  string searchString, 
+                                  int? page, 
+                                  int? show)
         {
             //sort strings
             ViewBag.CurrentSort = sortOrder;
@@ -96,13 +99,7 @@ namespace library_jkpg.Controllers
             int pageNumber = (page ?? 1);
             return View(books.ToPagedList(pageNumber, pageSize));
         }
-        //original method
-        //public ActionResult Index()
-        //{
-        //    var bOOKs = db.BOOKs.Include(b => b.CLASSIFICATION);
-        //    return View(bOOKs.ToList());
-        //}
-
+       
         // GET: Books/Details/5
         public ActionResult Details(string id)
         {
@@ -119,6 +116,7 @@ namespace library_jkpg.Controllers
         }
 
         // GET: Books/Create
+        [Authorize(Roles = "Administrator, Librarian")]
         public ActionResult Create()
         {
             ViewBag.SignId = new SelectList(db.CLASSIFICATIONs, "SignId", "Signum");
@@ -126,8 +124,7 @@ namespace library_jkpg.Controllers
         }
 
         // POST: Books/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Administrator, Librarian")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ISBN,Title,SignId,PublicationYear,publicationinfo,pages")] BOOK bOOK)
@@ -144,6 +141,7 @@ namespace library_jkpg.Controllers
         }
 
         // GET: Books/Edit/5
+        [Authorize(Roles = "Administrator, Librarian")]
         public ActionResult Edit(string id)
         {
             if (id == null)
@@ -158,10 +156,9 @@ namespace library_jkpg.Controllers
             ViewBag.SignId = new SelectList(db.CLASSIFICATIONs, "SignId", "Signum", bOOK.SignId);
             return View(bOOK);
         }
-
+        
         // POST: Books/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Administrator, Librarian")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ISBN,Title,SignId,PublicationYear,publicationinfo,pages")] BOOK bOOK)
@@ -177,6 +174,7 @@ namespace library_jkpg.Controllers
         }
 
         // GET: Books/Delete/5
+        [Authorize(Roles = "Administrator, Librarian")]
         public ActionResult Delete(string id)
         {
             if (id == null)
@@ -190,8 +188,9 @@ namespace library_jkpg.Controllers
             }
             return View(bOOK);
         }
-
+        
         // POST: Books/Delete/5
+        [Authorize(Roles = "Administrator, Librarian")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
